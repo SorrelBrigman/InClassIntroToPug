95b0fa982b71b2b776b5c6de7325155bd2aa3a14
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-28 10:54:03 -0500

    completed in class exercise

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+const names = [`Larry`, `Moe`, `Curly`, `Mr. Rogers`];
+
+app.get('/', (req, res, next)=>{
+  res.render('index', {subtitle: "This is data from my js", names, loggedIn: false})
+})
+
+app.get('/article', (req, res, next)=>{
+  res.render('article', {subtitle: "This is data from my js", names, loggedIn: true})
+})
+
+
+const port = process.env.PORT || 3000
+
+app.set('port', port);
+
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}`)
+})
